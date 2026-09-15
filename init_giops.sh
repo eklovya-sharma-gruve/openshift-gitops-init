c97fb21969aa3997ad126ee80d3cb7f1a0c5bb17
Eklovya Sharma <eklovya.sharma@gruve.ai>
2026-09-15 12:37:36 +0530

    modified gitops init script

--- init_giops.sh
+++ init_giops.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+read -p "CLUSTER_ADMIN_USER: " CLUSTER_ADMIN_USER
+read -p "CLUSTER_ADMIN_PASS: " CLUSTER_ADMIN_PASS
+# Login to cluster
+oc login \
+    -u "$CLUSTER_ADMIN_USER" \
+    -p "$CLUSTER_ADMIN_PASS" \
+    "https://api.${CLUSTER_NAME}.gruveai.com:6443"
+
 echo "Installing Openshift GitOps Operator"
 helm install gitops-operator ./argo-gitops-operator/ 
 
